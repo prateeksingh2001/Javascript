3d78b271f6dbf095e89427a4e0dc228f96a86aa6
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-12 11:23:23 +0000

    coversion1

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+//let score = 33
+let score = "33"
+let newScore= "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+console.log(typeof(newScore));
+
+let valueInNumber = Number(newScore);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber); //NaN -> Not a Number (output)
+
+let score1 = undefined   // NaN
+//let score2= true  -> 1
+// let score3= "Prateek" NaN
+console.log(typeof(Score1));
+
+let valueInNumber1= Number(newScore);
+console.log(typeof(valueInNumber1))
+
+console.log(valueInNumber); 
+
