311e56a6a1892ec3103f06442ee7092dfd01ed11
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-17 18:59:17 +0000

    dataTypes_Summary

---
+++ dataTypes_Summary.js
@@ -0,0 +1,55 @@
+// Primitive
+
+/* 7 Types : String ,
+             Number ,
+             Boolean ,
+             null -> Empty (Not Zero) ,
+             undefined -> Vaiable declared value not entered ,
+             Symbol -> Used to make value unique in advanced javascript front end development one component is used in many places some times we have to make it unique so we wrap it in a symbol and it bcmoes unique. ,
+             BigInt
+*/
+
+//Reference
+
+/* 
+        Arrays, Objects, Functions
+
+*/
+
+//const score = 100
+//In Js we don't have to define datatype
+
+//In Typescipt
+//const score:number = 100
+
+//Primitive
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 1246528461635633n //n represents bigInt
+
+//Reference
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+const myObj = {
+    name: "Prateek",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof bigNumber)
+console.log(typeof outsideTemp)
+console.log(typeof scoreValue)
+console.log(typeof myFunction) // object function bola jata hai
