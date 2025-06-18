62b48e4ac6235e01fa6c4e173bfd82afac385339
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-18 11:46:19 +0000

    Strings1

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name ="Prateek"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value") //old way
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); //New and better way
+
+//String Interpolation - we make placeholders variable is directly injected there
+
+const gameName = new String('prateeksingh')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.__proto__.length);
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
