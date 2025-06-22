ff2f55b334fcf5a6581110b8423795133fb57c76
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-22 14:01:46 +0000

    dates

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,15 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+ 
+
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toDateString());
