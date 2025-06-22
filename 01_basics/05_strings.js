aa33394fb8701aced33744ac426fb46f13a169ee
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-22 00:56:27 +0000

    strings in javascript

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,11 +8,32 @@ console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); //New 
 //String Interpolation - we make placeholders variable is directly injected there
 
 const gameName = new String('prateeksingh')
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-console.log(gameName.__proto__.length);
-console.log(gameName.length);
+// console.log(gameName.__proto__.length);
+// console.log(gameName.length);
 
-console.log(gameName.toUpperCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2)); //char at 2nd position(index)
+// console.log(gameName.indexOf('t')); // index of char
+
+// const newString = gameName.substring(0, 4); 
+// console.log(newString);
+
+//const anotherString = gameName.slice(2, 4);
+const anotherString = gameName.slice(-12, 3); //To be checked again Chatgpt
+console.log(anotherString);
+
+const newString1 = "      Prateek     " ;
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https:/prateek.com/prateek%20singh"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
