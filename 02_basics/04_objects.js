184880b229d3183408e4fcb4e95a21f74fbc8509
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-25 04:41:21 +0000

    04objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,64 @@
+// const tinderUser = new Object() //Singleton object
+
+const tinderUser = {} //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser ={
+    email: "some@gmail.com".at,
+    fullname: {
+        userfullname: {
+            firstname: "Darren",
+            lastname: "Sammy"
+        }
+    }
+}
+console.log(regularUser)
+console.log(regularUser.fullname)
+console.log(regularUser.fullname.userfullname.firstname)
+
+//console.log(regularUser.fullname?.userfullname.firstname)
+//? here used for condition if fullname exists then enter and access if not then leave
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2 } 
+//const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+//console.log(obj3)
+
+//objects in Array
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 2,
+        email: "abcd@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abcde@gmail.com"
+    }
+]
+
+//console.log(users[1].email)
+
+console.log(tinderUser); //data type is object
+
+console.log(Object.keys(tinderUser)); // datatype is array
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) //true
+console.log(tinderUser.hasOwnProperty('isLogged')) //false
