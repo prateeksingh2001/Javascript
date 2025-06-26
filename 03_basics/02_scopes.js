5f9593098a24833a63fbc514f66f2ba831869bd2
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-26 22:23:08 +0000

    scopes1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,32 @@
+
+// if(true){
+//     let a = 10;
+//     const b = 30
+//     var c = 40
+//     d = 50
+// }
+
+// console.log(a) // error
+// console.log(b) // error
+// console.log(c) // 40
+// console.log(d) // 50
+
+// inside {}-> block scope
+// outside -> global scope
+    var c = 500
+let a = 425;
+    const b = 745
+if(true){
+    let a = 10;
+    const b = 30
+    var c = 40
+    d = 50
+    console.log(a) 
+ console.log(b) 
+console.log(c) 
+ console.log(d)
+}
+
+console.log(a) 
+ console.log(b) 
+console.log(c) 
