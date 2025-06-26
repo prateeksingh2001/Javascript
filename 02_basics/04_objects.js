0259c5547f62fba89ac3fea63be36a52ca7df9ac
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-06-26 20:19:11 +0000

    destructuring_and_JSON_Api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,9 +17,9 @@ const regularUser ={
         }
     }
 }
-console.log(regularUser)
-console.log(regularUser.fullname)
-console.log(regularUser.fullname.userfullname.firstname)
+// console.log(regularUser)
+// console.log(regularUser.fullname)
+// console.log(regularUser.fullname.userfullname.firstname)
 
 //console.log(regularUser.fullname?.userfullname.firstname)
 //? here used for condition if fullname exists then enter and access if not then leave
@@ -53,12 +53,53 @@ const users = [
 
 //console.log(users[1].email)
 
-console.log(tinderUser); //data type is object
+// console.log(tinderUser); //data type is object
 
-console.log(Object.keys(tinderUser)); // datatype is array
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser)); // datatype is array
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')) //true
-console.log(tinderUser.hasOwnProperty('isLogged')) //false
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')) //true
+// console.log(tinderUser.hasOwnProperty('isLogged')) //false
+
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//console.log(course.courseInstructor);
+// const {courseInstructor} = course
+// console.log(courseInstructor);
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+/* In React
+const navbar = ({company}) => {    //proprs instead of props.company
+
+}
+
+navbar(company = "hitesh")
+*/
+
+// API apna kaam dusre ke sar pe daal dena 
+//fetching values 
+//Before the vlaues comes in XML format but now they come in json
+
+//JSON in boject form 
+// {
+//      courseName: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+//some Api are in array too
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+//fetch a random ali use json formatter to analyze it and understand it
