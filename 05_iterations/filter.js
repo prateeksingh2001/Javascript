01004c80b1f2e744d67154ff87ea587ed8b00ebf
prateeksingh2001 <88184803+prateeksingh2001@users.noreply.github.com>
2025-08-15 19:07:07 +0000

    filter_js

---
+++ 05_iterations/filter.js
@@ -0,0 +1,43 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//filter ke andar ek callback function milega jiske andar har value of access kiya jayega uske baad condition apply karenge jo us condition ko follow kaega usko return karenge
+//const newNums  = myNums.filter( (num) => num > 4)
+//console.log(newNums)
+
+//const nNums = myNums.filter( (num) => {
+//    return num > 4         //we have to use return keyword here
+//})
+//console.log(nNums)
+
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if( num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => {
+    return bk.genre === "History"
+  })
+
+  userBooks = books.filter( (bk) => bk.publish >= 1995)
+  
+  userBooks = books.filter( (bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
+  })
+  console.log(userBooks)
